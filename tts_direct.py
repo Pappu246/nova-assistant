bf004c798b85ca7f0b71cef8b1aeec4f6fef5c35
Pappu Yadav <pyadav32348@gmail.com>
2026-09-18 14:38:35 +0530

    Remove test files

--- tts_direct.py
+++
@@ -1,9 +0,0 @@
-﻿import edge_tts, asyncio, os
-async def gen():
-    c = edge_tts.Communicate(
-        "Namaste boss, main NOVA hoon. Aaj mausam bahut accha hai.",
-        "hi-IN-SwaraNeural", rate="+0%", pitch="+0Hz"
-    )
-    await c.save("C:/Users/pyada/Downloads/nova/test_voice.mp3")
-asyncio.run(gen())
-os.startfile("C:/Users/pyada/Downloads/nova/test_voice.mp3")
